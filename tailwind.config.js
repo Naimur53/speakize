d811760be4e261a050c34a5aad4aa992d8d0c293
naimur53 <naimurrhman53@gmail.com>
2023-02-19 00:48:26 +0600

    eopn

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "main-black": "#1E293B",
+        "inner-black": '#414042',
+        'dark-blue': '#0046A1',
+      },
+      spacing: {
+        sm: '8px',
+        md: '60px',
+        lg: '80px',
+        xl: '120px',
+      },
+      fontSize: {
+
+        'xxs': ['14px', { lineHeight: '21px', }],
+        'xs': ['16px', { lineHeight: '28px', }],
+
+        'ls': ['18px', { lineHeight: '24px', }],
+
+        'sm': ['20px', { lineHeight: '30px', }],
+        'normal': ['24px', { lineHeight: '28px', }],
+
+        'base': ['48px', { lineHeight: '72px', }],
+        'lg': ['60px', { lineHeight: '90px', }],
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '0px',
+        '2xl': '0px',
+      },
+      screens: {
+        'sm': '100%',
+        'md': '100%',
+        'lg': '1024px',
+        'xl': '1080px',
+        '2xl': '1080px',
+      }
+    },
+  },
+  plugins: [],
+}
